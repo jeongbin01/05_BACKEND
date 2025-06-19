76249b84413b0aa1a31eb841d80269c658a4123d
jeonjeongbin <01jjb@naver.com>
2025-06-19 14:14:15 +0900

    커밋

--- shop_thymeleaf1/src/main/java/com/shop/service/QuestionService.java
+++ shop_thymeleaf1/src/main/java/com/shop/service/QuestionService.java
@@ -1,12 +1,14 @@
 package com.shop.service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.shop.entity.Question;
@@ -53,8 +55,10 @@ public class QuestionService {
     //	질문 리스트 (페이징 처리함)
     public Page<Question> getList(int page) {
     	
-    	//
-        Pageable pageable = PageRequest.of(page, 10);
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+    	
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
         return this.questionRepository.findAll(pageable);
     }
 
